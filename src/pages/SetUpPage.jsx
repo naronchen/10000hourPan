5c5f0fc5920a99e746180597eaa0d64f1d8d1001
Naron <naron.chen@richmond.edu>
2023-12-20 21:23:04 +0800

    setUp page add button & slider

--- src/pages/SetUpPage.jsx
+++ src/pages/SetUpPage.jsx
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, {useState} from 'react'
+import '../css/setUpPage.css'
 
 export default function SetUpPage() {
+  const [inputValue, setInputValue] = useState('');
+  const [buttonClicked, setButtonClicked] = useState(false);
+  
+  const [inputContainers, setInputContainers] = useState([{
+    id: 1,
+    value: ''
+  }])
+
+  const handleAddInputContainer = () => {
+    setInputContainers([...inputContainers, {
+      id: inputContainers.length + 1,
+      value: ''
+    }])
+  }
+
+  const handleInputChange = (id, val) => {
+    const updatedContainers = inputContainers.map(container => {
+      if (container.id === id){
+        return {...container, value: val}
+      }
+      return container;
+    });
+    setInputContainers(updatedContainers);
+  }
+
+  const handleAddInput = () => {
+
+  }
+
   return (
-    <div>setUpPage</div>
+    <div className='setUp-container'>
+      <div className='input-container'>
+        <div className='text-title'>
+          输入目标
+        </div>
+
+        <div className='add-button-container'>
+          <button onClick={handleAddInputContainer} className='add-button'>+</button>
+          {/* <button onClick={handleMinus}>-</button> */}
+        </div>
+
+        {
+          inputContainers.map( container => (
+            <div key={container.id} className='singleInput-container'>
+              <input 
+                type='text' 
+                className='border-input' 
+                placeholder='你想要做什么？'
+                value={container.value}
+                onChange={(e)=>handleInputChange(container.id, e.target.value)}
+              />
+              <div className='button-container'>
+                <button className='input-button' onClick={handleAddInput()}></button>
+              </div>  
+            </div>
+          )
+          )
+        }
+      </div>
+      <div className='input-board'>
+
+      </div>
+    </div>
   )
 }
